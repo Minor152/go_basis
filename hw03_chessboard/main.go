682c1f3fc7a71162293c1b4a09362f844f31ce4f
Ruchik152 <info@nchernovn>
2024-08-27 23:51:18 +0300

    fix

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -2,34 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"strings"
 )
-
-var (
-	// Count colums.
-	SizeColumn int
-	// Count rows.
-	SizeRow int
-	// String containing a chessboard.
-	CHESSBOARD strings.Builder
-)
-
-func init() {
-	var err error
-
-	fmt.Println("Enter the number of rows: ")
-	_, err = fmt.Scanf("%d", &SizeRow)
-	if err != nil {
-		log.Fatalln(fmt.Errorf("%w", err))
-	}
-
-	fmt.Println("Enter the number of columns: ")
-	_, err = fmt.Scanf("%d", &SizeColumn)
-	if err != nil {
-		log.Fatalln(fmt.Errorf("%w", err))
-	}
-}
 
 func main() {
 	for l := 0; l < SizeRow; l++ {
@@ -40,6 +13,5 @@ func main() {
 			fmt.Fprintf(&CHESSBOARD, "# ")
 		}
 	}
-
 	fmt.Println(CHESSBOARD.String())
 }
