7354612650cbf273937ae34a6c2303205c4a2291
Ruchik152 <info@nchernovn>
2024-09-03 18:13:30 +0300

    Изменил main.go Убрал использование кастомной функции init Исправил замечания которые были указаны при прпроверке Использовал структуру.

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,17 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	for l := 0; l < SizeRow; l++ {
-		for c := 0; c < SizeColumn; c++ {
-			if c == 0 {
-				fmt.Fprintf(&CHESSBOARD, "\n")
-			}
-			fmt.Fprintf(&CHESSBOARD, "# ")
-		}
-	}
-	fmt.Println(CHESSBOARD.String())
+
+	var chessboard CHESSBOARD
+
+	chessboard.Init().Generate()
+
+	fmt.Println(chessboard.Print())
+
 }
